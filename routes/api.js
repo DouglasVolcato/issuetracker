00da54b0c0e76b437c7bf728c5d85158f1ee6195
Douglas Volcato <douglasvolcato@gmail.com>
2023-01-25 22:49:05 -0300

    fix: add getProjectsController route

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,12 @@
 const createProjectController = require("../controllers/createProject-controller");
 const deleteProjectController = require("../controllers/deleteProject-controller");
 const updateProjectController = require("../controllers/updateProject-controller");
+const getProjectsController = require("../controllers/getProjects-controller");
 
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
-    .get((req, res) => createProjectController(req, res))
+    .get((req, res) => getProjectsController(req, res))
     .post((req, res) => createProjectController(req, res))
     .put((req, res) => updateProjectController(req, res))
     .delete((req, res) => deleteProjectController(req, res));
